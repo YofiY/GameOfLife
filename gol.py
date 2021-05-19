d93864f50e3b27b2996a1ad1cecef8efcc5939ab
Vincent Palma <vincentpalma2001@gmail.com>
2021-05-19 17:12:06 +0200

    final version

--- gol.py
+++ gol.py
@@ -4,26 +4,50 @@ import tkinter as tk
 class Game:
     def __init__(self):
         self.ticks = 0
-        self.state = 0      # pause=0, play=1
-        self.zoom = 1       # the zoom scale, higher highest is 1
-        self.offset = [500,500]  # (x,y) offset in px
-        self.values = np.zeros((1000,1000),dtype=np.uint8) # matrix of values (1 or 0)
-        self.neighbours = np.zeros((1000,1000),dtype=np.uint8) # neighbours count of each cell
-        self.board = np.zeros((1000,1000),dtype=object) # corresponding board object
+        self.state = 0           # pause=0, play=1
+        self.sleep_time = 200   # sleep interval in ms
 
-        self.zoom = 1 #zoom state: 1 = 100%
+        self.grid_size = (25,40)
+
+        self.values = np.zeros(self.grid_size,dtype=np.uint8) # matrix of values (1 or 0)
+        self.neighbours = np.zeros(self.grid_size,dtype=np.uint8) # neighbours count of each cell
+        self.board = np.zeros(self.grid_size,dtype=object) # corresponding board object
+
+        self.screen_size_in_px = 1000 
 
     def main(self):
         self.GUI()
-        self.binders()
         self.root.mainloop()
 
-    def binders(self):
-        self.root.bind('<B1-Motion>', self.drag) #drag
-        self.root.bind('<Key>', self.keyboard_event_listener)
-        self.root.bind('<space>', self.setState)
+    def GUI(self):
+        self.root = tk.Tk()
+        self.root.title("Game of Life") 
+        self.canvas = tk.Canvas(self.root, width=self.screen_size_in_px, height=self.screen_size_in_px, bg="black")
+        self.options = tk.Canvas(self.root, width=200, height=50)
 
-    def setState(self, event):
+        self.play_btn = tk.Button(self.options, command=self.set_state, text="Play/Pause")
+        self.speed_up_btn = tk.Button(self.options, command=self.speed_up, text="Speed Up")
+        self.speed_down_btn = tk.Button(self.options, command=self.speed_down, text="Speed Down")
+        self.reset_btn = tk.Button(self.options, command=self.reset, text="Reset")
+
+        self.canvas.pack()
+        self.options.pack()
+
+        self.play_btn.pack()
+        self.speed_up_btn.pack()
+        self.speed_down_btn.pack()
+        self.reset_btn.pack()
+
+        self.draw()
+
+    def tick(self, n=1):
+        self.ticks += n
+        self.update()
+        print(self.ticks)
+        if self.state == 1:
+            self.root.after(self.sleep_time, self.tick)
+
+    def set_state(self):
         if self.state == 0:
             self.state =1
             self.tick()
@@ -32,19 +56,17 @@ class Game:
             self.state = 0
             print('PAUSE')
 
-    def drag(self, event):
-        print('clicked: {}'.format((event.x, event.y)))
-        return
+    def speed_up(self):
+        if self.sleep_time > 100: 
+            self.sleep_time -= 100
 
-    def keyboard_event_listener(self, event):
-        if event.keycode == 86 and self.zoom < 1: #86 = + keycode
-            self.zoom = round(1.25 * self.zoom, 4)
-            print('zoom: {}'.format(self.zoom))
+    def speed_down(self):
+        if self.sleep_time < 1000: 
+            self.sleep_time += 100
 
-        elif event.keycode == 82: #82 = - keycode
-            self.zoom = round(0.8 * self.zoom, 4)
-            print('dezoom: {}'.format(self.zoom))
-
+    def reset(self):
+        self.__init__()
+        self.draw()
 
     def click(self, event):
         info = event.widget.grid_info()
@@ -65,21 +87,21 @@ class Game:
         print('alive neighbours',self.neighbours[info['row']][info['column']])
 
     def draw(self):
-        for row in range(16):
-            for col in range(16):
-                if self.board[row+self.offset[0]][col+self.offset[1]] == 0: #if frame doesnt exist, create
-                    if not self.values[row+self.offset[0]][col+self.offset[1]]:
-                        self.board[row+self.offset[0]][col+self.offset[1]] = tk.Frame(self.canvas, bg='black', bd=0, width=(self.size_in_px/16), height=(self.size_in_px/16))
+        for row in range(self.grid_size[0]):
+            for col in range(self.grid_size[1]):
+                if self.board[row][col] == 0: #if frame doesnt exist, create
+                    if not self.values[row][col]:
+                        self.board[row][col] = tk.Button(self.canvas, bg='black', bd=0, width = 2, height = 1)
                     else:
-                        self.board[row+self.offset[0]][col+self.offset[1]] = tk.Frame(self.canvas, bg='white', bd=0, width=(self.size_in_px/16), height=(self.size_in_px/16))
+                        self.board[row][col] = tk.Button(self.canvas, bg='white', bd=0, width = 2, height = 1)
 
-                    self.board[row+self.offset[0]][col+self.offset[1]].grid( row=row+self.offset[0], column=col+self.offset[1])
-                    self.board[row+self.offset[0]][col+self.offset[1]].bind('<Button-1>', self.click)
+                    self.board[row][col].grid( row=row, column=col)
+                    self.board[row][col].bind('<Button-1>', self.click)
                 else:   #else update
-                    if not self.values[row+self.offset[0]][col+self.offset[1]]:
-                        self.board[row+self.offset[0]][col+self.offset[1]].config(bg='black')
+                    if not self.values[row][col]:
+                        self.board[row][col].config(bg='black')
                     else:
-                        self.board[row+self.offset[0]][col+self.offset[1]].config(bg='white')
+                        self.board[row][col].config(bg='white')
 
     def update_neighbours(self, died, row, col):
         for i in range(-1,2):
@@ -112,20 +134,6 @@ class Game:
             self.update_neighbours(el[0],el[1],el[2])
         self.draw()  
            
-
-    def GUI(self):
-        self.root = tk.Tk()
-        self.size_in_px = 640
-        self.canvas = tk.Canvas(self.root, width=self.size_in_px, height=self.size_in_px, bg="black")
-        self.canvas.pack()
-        self.draw()
-
-    def tick(self, n=1):
-        self.ticks += n
-        self.update()
-        print(self.ticks)
-        if self.state == 1:
-            self.root.after(200, self.tick)
 
 if __name__ == "__main__":
     Game().main()
