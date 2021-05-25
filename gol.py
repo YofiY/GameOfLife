e1c33785650eb82d6a4200ca26bc27d7d5bd4b97
Vincent Palma <vincentpalma2001@gmail.com>
2021-05-25 13:20:01 +0200

    clean up

--- gol.py
+++ gol.py
@@ -12,6 +12,7 @@ class Game:
 
         self.values = np.zeros(self.grid_size,dtype=np.uint8) # matrix of values (1 or 0)
         self.neighbours = np.zeros(self.grid_size,dtype=np.uint8) # neighbours count of each cell
+        self.cells = np.zeros(self.grid_size,dtype=object)
 
         self.screen_size_in_px = 1000 
 
@@ -28,7 +29,8 @@ class Game:
 
         self.play_btn = tk.Button(self.options, command=self.set_state, text="Play/Pause")
         self.speed = tk.Scale(self.options, command=self.set_speed, from_=5, to=1000, orient="horizontal")
-        self.open_btn = tk.Button(self.options, command=self.open, text="Select Pattern (RLE)")
+
+        self.open_file_btn = tk.Button(self.options, command=self.open_file, text="Select Pattern (RLE)")
         self.reset_btn = tk.Button(self.options, command=self.reset, text="Reset")
 
         self.canvas.pack()
@@ -36,7 +38,7 @@ class Game:
 
         self.play_btn.pack()
         self.speed.pack()
-        self.open_btn.pack()
+        self.open_file_btn.pack()
         self.reset_btn.pack()
 
         self.update()
@@ -45,7 +47,7 @@ class Game:
         if self.state:  #if play
             self.ticks += n
             self.update()
-            self.root.after(self.sleep_time, self.tick)
+            self.canvas.after(self.sleep_time, self.tick)
 
     #### Options ####
     def set_state(self):
@@ -55,76 +57,75 @@ class Game:
     def set_speed(self,value):
         self.sleep_time = int(value)
 
-    def open(self):     # browse https://www.conwaylife.com/wiki/Main_Page to download .RLE pattern files
+    def open_file(self):     # browse https://www.conwaylife.com/wiki/Main_Page to download .RLE pattern files
         fn = askopenfilename()
-        file = open(fn,"r")
-        lines = file.read().split('\n')
-        pattern, x, y = '','',''
-        for line in lines:
-            if line[0] != '#': #if its not a comment
-                if line[0] == 'x': # first line
-                    args = line.split(',')
-                    x = int(args[0].split(' ')[-1])
-                    y = int(args[1].split(' ')[-1])
-                else:
-                    pattern += line
-
-        a = []
-        for row in pattern.split('$'):
-            n = ''
-            r = []
-            for char in row:
-                if char == 'b':
-                    if n != '':
-                        r += int(n)*[0]
-                        n = ''
+        try:
+            file = open(fn,"r")
+            lines = file.read().split('\n')
+            pattern, x, y = '','',''
+            for line in lines:
+                if line[0] != '#': #if its not a comment
+                    if line[0] == 'x': # first line
+                        args = line.split(',')
+                        x = int(args[0].split(' ')[-1])
+                        y = int(args[1].split(' ')[-1])
                     else:
-                        r += [0]
-                elif char =='o':
-                    if n != '':
-                        r += int(n)*[1]
-                        n = ''
+                        pattern += line
+
+            a = []
+            for row in pattern.split('$'):
+                n = ''
+                r = []
+                for char in row:
+                    if char == 'b':
+                        if n != '':
+                            r += int(n)*[0]
+                            n = ''
+                        else:
+                            r += [0]
+                    elif char =='o':
+                        if n != '':
+                            r += int(n)*[1]
+                            n = ''
+                        else:
+                            r += [1]
+                    elif char =='!':
+                            r += (x-len(r))*[0]
                     else:
-                        r += [1]
-                elif char =='!':
-                        r += (x-len(r))*[0]
-                else:
-                    n += char
-            a.append(r)
+                        n += char
+                a.append(r)
 
-        pattern_values = np.array(a,dtype=np.uint8)
-        x_offset, y_offset = (self.grid_size[0]-y)//2, (self.grid_size[1]-x)//2
+            pattern_values = np.array(a,dtype=np.uint8)
+            x_offset, y_offset = (self.grid_size[0]-y)//2, (self.grid_size[1]-x)//2
 
-        self.values[x_offset:x_offset+pattern_values.shape[0], y_offset:y_offset+pattern_values.shape[1]] = pattern_values
-        self.values = np.rot90(np.fliplr(self.values),1)    #this is why the grid needs to be square, rot90 only works with square matrix
+            self.values[x_offset:x_offset+pattern_values.shape[0], y_offset:y_offset+pattern_values.shape[1]] = pattern_values
+            self.values = np.rot90(np.fliplr(self.values),1)    #this is why the grid needs to be square, rot90 only works with square matrix
 
-        for x in range(self.grid_size[0]):
-            for y in range(self.grid_size[1]):
-                if self.values[x][y]:
-                    self.draw_cell(x, y, 'white')
-                    self.update_neighbours(False, x, y)
+            for x in range(self.grid_size[0]):
+                for y in range(self.grid_size[1]):
+                    if self.values[x][y]:
+                        self.draw_cell(x, y, 'white')
+                        self.update_neighbours(False, x, y)
+        except: #user cancelled
+            pass
 
     def reset(self):
         self.__init__()
-        self.canvas.create_rectangle(0,0,self.screen_size_in_px,self.screen_size_in_px, fill='black', outline='black') # fill screen with black
+        self.canvas.delete("all")
 
     #### Game ####
     def draw_cell(self, x, y, color):
-        self.canvas.create_rectangle(x*(self.screen_size_in_px//self.grid_size[0]), y*(self.screen_size_in_px//self.grid_size[1]), (x+1)*(self.screen_size_in_px//self.grid_size[0]), (y+1)*(self.screen_size_in_px//self.grid_size[1]), fill=color, width=0, outline=color)
-    
+        if color == 'white':
+            self.cells[x][y] = self.canvas.create_rectangle(x*(self.screen_size_in_px//self.grid_size[0]), y*(self.screen_size_in_px//self.grid_size[1]), (x+1)*(self.screen_size_in_px//self.grid_size[0]), (y+1)*(self.screen_size_in_px//self.grid_size[1]), fill=color, width=0, outline=color)
+        else:
+            self.canvas.delete(self.cells[x][y])
+
     def binders(self):
         self.canvas.bind('<B1-Motion>', self.drag) #drag
         self.canvas.bind('<Button-1>', self.click)
 
-    def drag(self, event):
-        x,y = int((event.x/self.screen_size_in_px)*self.grid_size[0]), int((event.y/self.screen_size_in_px)*self.grid_size[1])
-        if self.values[x][y] == 0: # for drag, only activate cells
-            self.values[x][y] = 1
-            self.draw_cell(x, y, 'white')
-            self.update_neighbours(False, x, y)
-
     def click(self, event):
-        x,y = int((event.x/self.screen_size_in_px)*self.grid_size[0]), int((event.y/self.screen_size_in_px)*self.grid_size[1])  # something like that
+        x,y = int((event.x/self.screen_size_in_px)*self.grid_size[0]), int((event.y/self.screen_size_in_px)*self.grid_size[1])
 
         if self.values[x][y] == 0:
             self.values[x][y] = 1
@@ -134,6 +135,16 @@ class Game:
             self.values[x][y] = 0
             self.draw_cell(x, y, 'black')
             self.update_neighbours(True, x, y)
+
+    def drag(self, event):
+        x,y = int((event.x/self.screen_size_in_px)*self.grid_size[0]), int((event.y/self.screen_size_in_px)*self.grid_size[1])
+        try:
+            if self.values[x][y] == 0: # for drag, only activate cells
+                self.values[x][y] = 1
+                self.draw_cell(x, y, 'white')
+                self.update_neighbours(False, x, y)
+        except IndexError:
+            pass
 
     def update_neighbours(self, died, x, y):
         for i in range(-1,2):
@@ -162,8 +173,7 @@ class Game:
             to_update.append([True,x,y])
         
         for el in to_update:    # update neighbours only on at the end
-            x,y = el[1],el[2]
-            self.update_neighbours(el[0],x,y)
+            self.update_neighbours(el[0],el[1],el[2])
                 
 if __name__ == "__main__":
     Game().main()
